4afa8787f92a7802891b7d56931732663f8d866a
Niek <niek_vs@hotmail.com>
2018-11-22 15:08:24 +0100

    loading data from db at startup

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import './firebase/firebase';
 
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 
@@ -16,8 +16,14 @@ const store = configureStore();
 const jsx = (
     <Provider store={store}>
         <AppRouter />
-    </Provider >
+    </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(()=>{
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
+
 
